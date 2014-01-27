c35fc2af8cdea59988de944b83c98dc94e59f64f
Joe Koberg <joe.koberg@versionone.com>
2014-01-27 10:38:48 -0500

    Include generated assemblyinfo.fs in project

---
+++ VersionOne.Parsers/AssemblyInfo.fs
@@ -0,0 +1,14 @@
+﻿module VersionOne.Parsers.AssemblyInfo
+
+open System.Reflection
+
+[<assembly: AssemblyTitle("VersionOne Parsers Library")>]
+[<assembly: AssemblyConfiguration("Release")>]
+[<assembly: AssemblyCompany("VersionOne, Inc.")>]
+[<assembly: AssemblyProduct("VersionOne.Parsers")>]
+[<assembly: AssemblyCopyright("Copyright 2013-2014, VersionOne, Inc. Please see the LICENSE.MD file.")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.1029")>]
+
+ignore ()
+
